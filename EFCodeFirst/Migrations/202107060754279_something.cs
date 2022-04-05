9f38ace7b7f940a9341c5788827c28da6a20b160
phong200200 <quocphong20022000@gmail.com>
2022-04-05 23:28:16 +0700

    Demogit

--- EFCodeFirst/Migrations/202107060754279_something.cs
+++ EFCodeFirst/Migrations/202107060754279_something.cs
@@ -6,103 +6,7 @@ namespace EFCodeFirst.Migrations
     
     public partial class something : DbMigration
     {
-        public override void Up()
-        {
-            CreateTable(
-                "dbo.Course",
-                c => new
-                    {
-                        CourseID = c.Int(nullable: false),
-                        Title = c.String(maxLength: 50),
-                        Credits = c.Int(nullable: false),
-                        DepartmentID = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.CourseID)
-                .ForeignKey("dbo.Department", t => t.DepartmentID, cascadeDelete: true)
-                .Index(t => t.DepartmentID);
-            
-            CreateTable(
-                "dbo.Department",
-                c => new
-                    {
-                        DepartmentID = c.Int(nullable: false, identity: true),
-                        Name = c.String(maxLength: 50),
-                        Budget = c.Decimal(nullable: false, storeType: "money"),
-                        StartDate = c.DateTime(nullable: false),
-                        InstructorID = c.Int(),
-                    })
-                .PrimaryKey(t => t.DepartmentID)
-                .ForeignKey("dbo.Instructor", t => t.InstructorID)
-                .Index(t => t.InstructorID);
-            
-            CreateTable(
-                "dbo.Instructor",
-                c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        LastName = c.String(nullable: false, maxLength: 50),
-                        FirstName = c.String(nullable: false, maxLength: 50),
-                        HireDate = c.DateTime(nullable: false),
-                    })
-                .PrimaryKey(t => t.ID);
-            
-            CreateTable(
-                "dbo.OfficeAssignment",
-                c => new
-                    {
-                        InstructorID = c.Int(nullable: false),
-                        Location = c.String(maxLength: 50),
-                    })
-                .PrimaryKey(t => t.InstructorID)
-                .ForeignKey("dbo.Instructor", t => t.InstructorID)
-                .Index(t => t.InstructorID);
-            
-            CreateTable(
-                "dbo.Enrollment",
-                c => new
-                    {
-                        EnrollmentID = c.Int(nullable: false, identity: true),
-                        CourseID = c.Int(nullable: false),
-                        StudentID = c.Int(nullable: false),
-                        Grade = c.Int(),
-                    })
-                .PrimaryKey(t => t.EnrollmentID)
-                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
-                .ForeignKey("dbo.Student", t => t.StudentID, cascadeDelete: true)
-                .Index(t => t.CourseID)
-                .Index(t => t.StudentID);
-            
-            CreateTable(
-                "dbo.Student",
-                c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        LastName = c.String(nullable: false, maxLength: 50),
-                        FirstName = c.String(nullable: false, maxLength: 50),
-                        EnrollmentDate = c.DateTime(nullable: false),
-                    })
-                .PrimaryKey(t => t.ID);
-            
-            CreateTable(
-                "dbo.CourseInstructor",
-                c => new
-                    {
-                        CourseID = c.Int(nullable: false),
-                        InstructorID = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => new { t.CourseID, t.InstructorID })
-                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
-                .ForeignKey("dbo.Instructor", t => t.InstructorID, cascadeDelete: true)
-                .Index(t => t.CourseID)
-                .Index(t => t.InstructorID);
-                // Create  a department for course to point to.
-                Sql("INSERT INTO dbo.Department (Name, Budget, StartDate) VALUES ('Temp', 0.00, GETDATE())");
-                //  default value for FK points to department created above.
-                AddColumn("dbo.Course", "DepartmentID", c => c.Int(nullable: false, defaultValue: 1));
-            //AddColumn("dbo.Course", "DepartmentID", c => c.Int(nullable: false));
-            AlterColumn("dbo.Course", "Title", c => c.String(maxLength: 50));
-        }
-
+        
         public override void Down()
         {
             DropForeignKey("dbo.CourseInstructor", "InstructorID", "dbo.Instructor");
@@ -127,5 +31,103 @@ namespace EFCodeFirst.Migrations
             DropTable("dbo.Department");
             DropTable("dbo.Course");
         }
+
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Course",
+                c => new
+                {
+                    CourseID = c.Int(nullable: false),
+                    Title = c.String(maxLength: 50),
+                    Credits = c.Int(nullable: false),
+                    DepartmentID = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.CourseID)
+                .ForeignKey("dbo.Department", t => t.DepartmentID, cascadeDelete: true)
+                .Index(t => t.DepartmentID);
+
+            CreateTable(
+                "dbo.Department",
+                c => new
+                {
+                    DepartmentID = c.Int(nullable: false, identity: true),
+                    Name = c.String(maxLength: 50),
+                    Budget = c.Decimal(nullable: false, storeType: "money"),
+                    StartDate = c.DateTime(nullable: false),
+                    InstructorID = c.Int(),
+                })
+                .PrimaryKey(t => t.DepartmentID)
+                .ForeignKey("dbo.Instructor", t => t.InstructorID)
+                .Index(t => t.InstructorID);
+
+            CreateTable(
+                "dbo.Instructor",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    LastName = c.String(nullable: false, maxLength: 50),
+                    FirstName = c.String(nullable: false, maxLength: 50),
+                    HireDate = c.DateTime(nullable: false),
+                })
+                .PrimaryKey(t => t.ID);
+
+            CreateTable(
+                "dbo.OfficeAssignment",
+                c => new
+                {
+                    InstructorID = c.Int(nullable: false),
+                    Location = c.String(maxLength: 50),
+                })
+                .PrimaryKey(t => t.InstructorID)
+                .ForeignKey("dbo.Instructor", t => t.InstructorID)
+                .Index(t => t.InstructorID);
+
+            CreateTable(
+                "dbo.Enrollment",
+                c => new
+                {
+                    EnrollmentID = c.Int(nullable: false, identity: true),
+                    CourseID = c.Int(nullable: false),
+                    StudentID = c.Int(nullable: false),
+                    Grade = c.Int(),
+                })
+                .PrimaryKey(t => t.EnrollmentID)
+                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
+                .ForeignKey("dbo.Student", t => t.StudentID, cascadeDelete: true)
+                .Index(t => t.CourseID)
+                .Index(t => t.StudentID);
+
+            CreateTable(
+                "dbo.Student",
+                c => new
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    LastName = c.String(nullable: false, maxLength: 50),
+                    FirstName = c.String(nullable: false, maxLength: 50),
+                    EnrollmentDate = c.DateTime(nullable: false),
+                })
+                .PrimaryKey(t => t.ID);
+
+            CreateTable(
+                "dbo.CourseInstructor",
+                c => new
+                {
+                    CourseID = c.Int(nullable: false),
+                    InstructorID = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => new { t.CourseID, t.InstructorID })
+                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
+                .ForeignKey("dbo.Instructor", t => t.InstructorID, cascadeDelete: true)
+                .Index(t => t.CourseID)
+                .Index(t => t.InstructorID);
+            // Create  a department for course to point to.
+            Sql("INSERT INTO dbo.Department (Name, Budget, StartDate) VALUES ('Temp', 0.00, GETDATE())");
+            //  default value for FK points to department created above.
+            AddColumn("dbo.Course", "DepartmentID", c => c.Int(nullable: false, defaultValue: 1));
+            //AddColumn("dbo.Course", "DepartmentID", c => c.Int(nullable: false));
+            AlterColumn("dbo.Course", "Title", c => c.String(maxLength: 50));
+        }
+
     }
 }
